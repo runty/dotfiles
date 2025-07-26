827c9825e2b7336425e89927291f28ff12927d4c
Phil Wu <phil@runty.com>
2025-07-26 15:06:20 -0700

    update hyprland

--- hypr/.config/hypr/setwallpaper.sh
+++ hypr/.config/hypr/setwallpaper.sh
@@ -3,7 +3,7 @@
 hyprctl hyprpaper unload all
 # Sets a random wallpaper with hyprpaper
 
-wall=$( find "/mnt/cloud/GDrive/Stuff/Wallpaper/arcanefavs/" -type f -print0 | shuf -z -n 1 )
+wall=$( find "/home/phobus/walls/" -type f -print0 | shuf -z -n 1 )
 
 hyprctl hyprpaper preload $wall
 hyprctl hyprpaper wallpaper ,$wall
